66af73434c0ec8a76f0149bd77f703a01a8a1d69
Cole Seifert <seiferco@oregonstate.edu>
2025-05-12 12:01:38 -0700

    Final Version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CS_370_project-3
+Note: Unable to test this with anything else besides IOS
+To run this program you must start by 
+1. Installing all the dependecies ithin "requirements.txt".
+
+To create a QR code containing the URI follow these steps: 
+1. In the command line enter: python ./submission --generate-qr
+2. Referesh your working directory and open the file "qr_code.png"
+3. Scan the qr code with Google Authenticator (GA)
+Note: This will create your secret key in the working directory under "secret_key.txt"
+
+To print the current GA OTP follow these steps:
+1. In the command line enter: python ./submission --get-otp
+2. This will print the current OTP every 30 seconds until user exits via CTRL+C or kills the program.
--- submission.py
+++ submission.py
@@ -1,25 +1,42 @@
 from sys import argv
 import qrcode
 import pyotp
+import time
+import base64
 
-def generate_QR(key):
+def generate_QR():
+    # Generate a new key and save it to a file
+    key = pyotp.random_base32()
+    with open("secret_key.txt", 'w') as file:
+        file.write(key)
 
+    # Create QR for user to scan with Google Authenticator 
     URI = pyotp.totp.TOTP(key).provisioning_uri(name="Seiferco", issuer_name="Project-3 Program")
     qrcode.make(URI).save("qr_code.png")
     
     return
 
 def get_OTP():
+    
+    # Retrieve the key that was created for TOTP
+    with open("secret_key.txt", 'r') as file:
+        key = file.read()
+
+    
+    # Print current valid TOTP every 30 seconds
+    while(1):
+        totp = pyotp.TOTP(key)
+        print(f"Current OTP: {totp.now()}")
+        time.sleep(30)
 
     return
 
 def main():
-    key = pyotp.random_base32()
     
     command = argv[1]
 
     if(command == "--generate-qr"):
-        generate_QR(key)
+        generate_QR()
 
     elif(command == "--get-otp"):
         get_OTP()
